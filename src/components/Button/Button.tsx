ab84eb0782c5c9b15bda1b4ba3108c13005245af
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-05-08 14:07:42 +0600

    Login and signup screen created

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,50 @@
+import {StyleSheet, Text, Pressable, StyleProp} from 'react-native';
+import React from 'react';
+interface ButtonProps {
+  children?: React.ReactNode;
+}
+interface ButtonProps {
+  title?: string;
+  style?: StyleProp<any>;
+  variant?: string;
+  onPress?: () => void;
+}
+const Button: React.FC<ButtonProps> = ({
+  title,
+  style,
+  variant = 'primary',
+  onPress,
+  children,
+}: ButtonProps) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({pressed}) => [
+        styles.button,
+        style,
+        pressed ? {opacity: 0.5} : {opacity: 1},
+      ]}>
+      {!children && (
+        <Text
+          style={[styles.text, variant !== 'primary' ? {color: 'black'} : {}]}>
+          {title}
+        </Text>
+      )}
+      {children}
+    </Pressable>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#3B82F6',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    color: 'white',
+  },
+});
